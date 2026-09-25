9a7ad0f641fdc959327a0c4b8c3f659fab3ecef7
harshithchandarsenthu <harshithchandarsenthu@gmail.com>
2026-09-25 20:48:49 +0530

    Update Activity 1.py

--- Activity 1.py
+++ Activity 1.py
@@ -1,5 +1,11 @@
 print("This is a statement", )
 # This is a comment 
-print("/n Line spacing")
+print("\n Line spacing")
 a = 32
 print("The number is", a)
+print("Welcome to the world of programming!")
+print(4)
+print("Hello Python \n")
+print("Keywords in Python are....")
+import keyword
+print(keyword.kwlist)
